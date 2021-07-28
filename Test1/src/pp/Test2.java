2ede6f1ef8899434327e27b30924c841f1a7e6b3
Guest <Guest@localhost>
2021-07-28 15:57:14 +0200

    3

--- Test1/src/pp/Test2.java
+++ Test1/src/pp/Test2.java
@@ -1,6 +1,10 @@
 package pp;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 public class Test2 {
 
@@ -10,10 +14,10 @@ public class Test2 {
 		ArrayList<Gasto> lista = new ArrayList<Gasto>();
 
 		lista.add(new Gasto("A", 80, "uno"));
-		lista.add(new Gasto("B", 50, "uno"));
-		lista.add(new Gasto("C", 70, "uno"));
-		lista.add(new Gasto("D", 95, "dos"));
-		lista.add(new Gasto("F", 95, "uno"));
+		lista.add(new Gasto("D", 50, "uno"));
+		lista.add(new Gasto("B", 70, "uno"));
+		lista.add(new Gasto("E", 95, "dos"));
+		lista.add(new Gasto("C", 95, "uno"));
 
 		double totalPago = 0;
 
@@ -38,6 +42,12 @@ public class Test2 {
 
 		// anyMatch si cualquiera coincide
 		System.out.println(lista.stream().anyMatch(n -> n.getClave().compareToIgnoreCase("dos") == 0));
+
+		lista.stream().sorted(Comparator.comparing(Gasto::getNombre)).forEach(n -> System.out.println(n.getNombre()));
+
+//		lista.stream().sorted().forEach(n -> System.out.println(n.getNombre()));
+
+		lista.stream().forEach(n -> System.out.println(n.getNombre()));
 
 	}
 
