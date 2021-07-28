6ee20b2e2062a1613bd653c46f12afa167d6142c
dpaulmt <dpaul.mt@hotmail.com>
2021-07-28 21:07:45 +0200

    Add files via upload

---
+++ Test1/src/pp/P2.java
@@ -0,0 +1,36 @@
+package pp;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class P2 {
+
+	public static void main(String[] args) {
+
+		List<String> codeList = new ArrayList<String>();
+
+		codeList.add("orange");
+		codeList.add("apple apple");
+		codeList.add("banana orange apple");
+		codeList.add("banana");
+
+		List<String> shoppingList = new ArrayList<String>();
+
+		shoppingList.add("orange");
+		shoppingList.add("apple");
+		shoppingList.add("apple");
+		shoppingList.add("banana");
+		shoppingList.add("orange");
+		shoppingList.add("apple");
+		shoppingList.add("banana");
+
+	}
+
+	static boolean foo(List<String> codeList, List<String> shoppingList) {
+
+		return false;
+	}
+
+}
